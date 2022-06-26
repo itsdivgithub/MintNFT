34c72a132103c5e9084e4214aac7194763ec71bd
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2022-06-26 22:49:13 +0530

    Done

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+    //0x48c3B7F9A43984d08Be1FEacEA4901CAd04304d4
